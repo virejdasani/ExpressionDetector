225b2bb7a533fe8a40037824aafc6013a9893ee9
Virej Dasani <dasanivirej@gmail.com>
2021-04-16 12:12:24 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+// To get the video stream from the camera
+function startVideo() {
+    navigator.getUserMedia({
+            video: {}
+        },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {
+        width: video.width,
+        height: video.height
+    }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(01, 10, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
+})
+
+startVideo()
